3e141431de241706a78844bab621b50f3e61cf67
Ajaykumar <ajaykumarbalusu77@gmail.com>
2026-07-14 12:39:16 +0530

    Arguments script

---
+++ scripts/arguments.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "The name of this script is: $0"
+echo "The first argument is: $1"
+echo "The second argument is: $2"
+echo "Number of arguments passed: $#"
+echo "All the arguments passed - as SINGLE word: $*"
+echo "All the arguments passed - as individual words: $@"
+echo "Exit status is: $?"
